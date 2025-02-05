f591cc3bdddf90610a511cd159f752abc551bf9f
MahakRajwani112004 <mahak@ssipmt.com>
2025-02-05 14:00:17 +0530

    add : port env

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.post("/generate", generateResponse);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT ;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
