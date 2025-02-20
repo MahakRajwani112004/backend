0a04ea2cbb8b683a0c7b3e39b74cf356bb211f3e
MahakRajwani112004 <mahak@ssipmt.com>
2025-02-20 17:41:23 +0530

    Add : Google tts

--- src/services/audio/audio.service.ts
+++ src/services/audio/audio.service.ts
@@ -1,27 +1,29 @@
-import { ElevenLabsClient } from "elevenlabs";
+import * as googleTTS from "google-tts-api";
+import axios from "axios";
 
 export const generateAudioBase64 = async (text: string): Promise<string> => {
-  const client = new ElevenLabsClient();
-
+  if (!text) {
+    throw new Error("Text is required");
+  }
   try {
-    const audioStream = await client.textToSpeech.convert(
-      "JBFqnCBsd6RMkjVDRZzb",
-      {
-        text: text,
-        model_id: "eleven_multilingual_v2",
-        output_format: "mp3_44100_128",
-      }
+    const urls = googleTTS.getAllAudioUrls(text, {
+      lang: "En",
+      slow: false,
+      host: "https://translate.google.com",
+    });
+
+    const audioBase64Parts = await Promise.all(
+      urls.map(async (urlObj) => {
+        const response = await axios.get(urlObj.url, {
+          responseType: "arraybuffer",
+        });
+        return Buffer.from(response.data, "binary").toString("base64");
+      })
     );
-
-    const chunks: Uint8Array[] = [];
-    for await (const chunk of audioStream) {
-      chunks.push(chunk);
-    }
-
-    const audioBuffer = Buffer.concat(chunks);
-    return audioBuffer.toString("base64");
+    const combinedAudioBase64 = audioBase64Parts.join("");
+    return combinedAudioBase64;
   } catch (error) {
-    console.error("Error generating audio:", error);
-    throw new Error("Audio generation failed");
+    console.error("Error:", error);
+    throw new Error("Failed to generate audio");
   }
 };
